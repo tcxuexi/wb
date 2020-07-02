d6322c0bfb10f21a00c51ef891da3804e905ea28
tcxuexi <xuexi1998@163.com>
2020-07-02 12:51:00 +0800

    优化页面，用户注册

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,19 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title', 'Wb-APP')</title>
+  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-    <div class="container">
-      @yield('content')
-    </div>
+
+@include('layouts._header')
+
+<div class="container">
+  <div class="offset-md-1 col-md-10">
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get(
     }
 );
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('about', 'StaticPagesController@about');
-Route::get('help', 'StaticPagesController@help');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('about', 'StaticPagesController@about')->name('about');
+Route::get('help', 'StaticPagesController@help')->name('help');
+
+Route::get('/signup', 'UserController@create')->name('signup');
