32cfb5926edde31243bd0fc610affd006b0df34d
tcxuexi <xuexi1998@163.com>
2020-07-02 15:31:33 +0800

    用户注册，表单，验证器，结果反馈，语言包，User::create[],session->flush('',''),redirect()->route('', [])

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('about', 'StaticPagesController@about')->name('about');
 Route::get('help', 'StaticPagesController@help')->name('help');
 
-Route::get('/signup', 'UserController@create')->name('signup');
+Route::get('/signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+
